e272a102d821be9ed434daa65e89c25bb265b40c
Yu Ishikawa <yu-iskw@users.noreply.github.com>
2022-06-30 23:33:53 +0900

    Set up the testing workflow (#2)
    * Set up the testing workflow
    
    Signed-off-by: Yu ISHIKAWA <yuu.ishikawa@gmail.com>
    
    * Change the trigger rule
    
    Signed-off-by: Yu ISHIKAWA <yuu.ishikawa@gmail.com>
    
    * Change the trigger rule again
    
    Signed-off-by: Yu ISHIKAWA <yuu.ishikawa@gmail.com>
    
    * Fix the workflow
    
    Signed-off-by: Yu ISHIKAWA <yuu.ishikawa@gmail.com>
    
    * Set the env
    
    Signed-off-by: Yu ISHIKAWA <yuu.ishikawa@gmail.com>
    
    * Update
    
    Signed-off-by: Yu ISHIKAWA <yuu.ishikawa@gmail.com>
    
    * Persist artifacts for debug
    
    Signed-off-by: Yu ISHIKAWA <yuu.ishikawa@gmail.com>
    
    * Improve to persist artifacts
    
    Signed-off-by: Yu ISHIKAWA <yuu.ishikawa@gmail.com>
    
    * Add README.md in integration_tests
    
    Signed-off-by: Yu ISHIKAWA <yuu.ishikawa@gmail.com>

---
+++ integration_tests/macros/tests/bigquery/pseudonymization/test_sha256.sql
@@ -0,0 +1,8 @@
+{% macro test_sha256() %}
+  {{- return(adapter.dispatch("test_sha256", "dbt_data_privacy_integration_tests")()) -}}
+{% endmacro %}
+
+{% macro bigquery__test_sha256() %}
+  {%- set result = dbt_data_privacy.sha256("dummy_column") -%}
+  {{ assert_equals(result, "TO_BASE64(SHA256(CAST(dummy_column AS STRING)))") }}
+{% endmacro %}
