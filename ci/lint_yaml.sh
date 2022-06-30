bca2972522dc71a5cf75fce0086367c492baf568
Yu ISHIKAWA <yuu.ishikawa@gmail.com>
2022-06-30 18:03:23 +0900

    Add a GitHub workflow to check styles
    Signed-off-by: Yu ISHIKAWA <yuu.ishikawa@gmail.com>

--- ci/lint_yaml.sh
+++ ci/lint_yaml.sh
@@ -7,5 +7,5 @@ set -e
 SCRIPT_DIR="$(dirname "$(readlink -f "$0")")"
 PROJECT_DIR="$(dirname "$SCRIPT_DIR")"
 
-yamllint -s "${PROJECT_DIR}"
-yamllint -s "${PROJECT_DIR}"/macros
+yamllint -s "${PROJECT_DIR}/dbt_project.yml"
+yamllint -s "${PROJECT_DIR}/macros/"
