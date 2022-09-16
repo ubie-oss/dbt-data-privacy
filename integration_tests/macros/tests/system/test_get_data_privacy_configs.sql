4b4c25908a708791ee9c415b6e2286a20fe7ff81
Yu ISHIKAWA <yuu.ishikawa@gmail.com>
2022-09-16 16:32:06 +0900

    Fix
    Signed-off-by: Yu ISHIKAWA <yuu.ishikawa@gmail.com>

--- integration_tests/macros/tests/system/test_get_data_privacy_configs.sql
+++ integration_tests/macros/tests/system/test_get_data_privacy_configs.sql
@@ -1,16 +1,5 @@
 {% macro test_get_data_privacy_configs() %}
   {% set result = dbt_data_privacy.get_data_privacy_configs() %}
-  {% set expected = {
-      'data_analysis': {
-        'default_materialization': 'view',
-        'data_handling_standards': {
-          'public': { 'method': 'RAW' },
-          'internal': {'method': 'RAW'},
-          'confidential': {'method': 'SHA256', 'converted_level': 'internal'},
-          'restricted': {'method': 'DROPPED'}
-        }
-      }
-   } %}
-
+  {% set expected = var('data_privacy', {}) %}
   {{ assert_dict_equals(result, expected) }}
 {% endmacro %}
