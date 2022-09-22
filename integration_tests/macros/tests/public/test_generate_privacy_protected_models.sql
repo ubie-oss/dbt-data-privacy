1963fadf1aaf57c0889f4de950f8c459a7ff3cde
Yu ISHIKAWA <yuu.ishikawa@gmail.com>
2022-09-22 14:35:59 +0900

    Enhance `generate_privacy_protected_models`
    Signed-off-by: Yu ISHIKAWA <yuu.ishikawa@gmail.com>

--- integration_tests/macros/tests/public/test_generate_privacy_protected_models.sql
+++ integration_tests/macros/tests/public/test_generate_privacy_protected_models.sql
@@ -11,4 +11,17 @@
     {{ assert_element_in_list("model_sql", generated_model) }}
     {{ assert_element_in_list("schema_yaml", generated_model) }}
   {% endfor %}
+
+  {% set target_unique_ids = [
+      "model.dbt_data_privacy_integration_tests.restricted_layer__dbt_data_privacy_restricted_layer__consents",
+      "model.dbt_data_privacy_integration_tests.restricted_layer__dbt_data_privacy_restricted_layer__items",
+  ] %}
+  {% set generated_models = dbt_data_privacy.generate_privacy_protected_models(unique_ids=target_unique_ids, verbose=false) %}
+  {{ assert_equals(generated_models | length, 2) }}
+  {% for generated_model in generated_models %}
+    {{ assert_element_in_list("name", generated_model) }}
+    {{ assert_element_in_list("meta", generated_model) }}
+    {{ assert_element_in_list("model_sql", generated_model) }}
+    {{ assert_element_in_list("schema_yaml", generated_model) }}
+  {% endfor %}
 {% endmacro %}
--- macros/public/generate_privacy_protected_models.sql
+++ macros/public/generate_privacy_protected_models.sql
@@ -1,4 +1,4 @@
-{% macro generate_privacy_protected_models(verbose=true) %}
+{% macro generate_privacy_protected_models(unique_ids=none, verbose=true) %}
   {% set generated_models = [] %}
 
   {# Generate dbt models and sources #}
@@ -6,20 +6,23 @@
   {% do models_and_sources.extend(dbt_data_privacy.get_nodes("model")) %}
   {% do models_and_sources.extend(dbt_data_privacy.get_nodes("source")) %}
 
-  {% if models_and_sources | length == 0 %}
-    {% do exceptions.warn("Found no models and sources") %}
+  {# Set the default value with all models which have an 'unique_id' attribute. #}
+  {% if unique_ids is none %}
+    {% set unique_ids = models_and_sources | selectattr("unique_id", "defined") | map(attribute="unique_id") | list %}
   {% endif %}
 
-  {% if execute and models_and_sources | length > 0 %}
-    {% for model_or_source in models_and_sources %}
-      {% if dbt_data_privacy.has_data_privacy_meta(model_or_source) %}
-        {% set generated_model = dbt_data_privacy.generate_privacy_protected_model(model_or_source) %}
-        {% if generated_model is not none and generated_model | length > 0 %}
-          {{ generated_models.extend(generated_model) }}
-        {% endif %}
-      {% endif %}
-    {% endfor %}
+  {% if unique_ids | length == 0 %}
+    {{ exceptions.raise_compiler_error("There is no 'unique_id's to select.") }}
   {% endif %}
+
+  {% for model_or_source in models_and_sources %}
+    {% if model_or_source.unique_id in unique_ids and dbt_data_privacy.has_data_privacy_meta(model_or_source) %}
+      {% set generated_model = dbt_data_privacy.generate_privacy_protected_model(model_or_source) %}
+      {% if generated_model is not none and generated_model | length > 0 %}
+        {{ generated_models.extend(generated_model) }}
+      {% endif %}
+    {% endif %}
+  {% endfor %}
 
   {% if verbose is true %}
     {% do print(tojson(generated_models)) %}
