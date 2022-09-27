831e5d44b1e5a846ce2d3845a30fe87de47931f8
Yu ISHIKAWA <yuu.ishikawa@gmail.com>
2022-09-28 08:43:12 +0900

    Add the `original_file_paths` arg
    Signed-off-by: Yu ISHIKAWA <yuu.ishikawa@gmail.com>

--- macros/public/generate_privacy_protected_models.sql
+++ macros/public/generate_privacy_protected_models.sql
@@ -1,4 +1,4 @@
-{% macro generate_privacy_protected_models(unique_ids=none, verbose=true) %}
+{% macro generate_privacy_protected_models(unique_ids=none, original_file_paths=none, verbose=true) %}
   {% set generated_models = [] %}
 
   {# Generate dbt models and sources #}
@@ -6,23 +6,41 @@
   {% do models_and_sources.extend(dbt_data_privacy.get_nodes("model")) %}
   {% do models_and_sources.extend(dbt_data_privacy.get_nodes("source")) %}
 
-  {# Set the default value with all models which have an 'unique_id' attribute. #}
-  {% if unique_ids is none %}
-    {% set unique_ids = models_and_sources | selectattr("unique_id", "defined") | map(attribute="unique_id") | list %}
-  {% endif %}
-
-  {% if unique_ids | length == 0 %}
-    {{ exceptions.raise_compiler_error("There is no 'unique_id's to select.") }}
-  {% endif %}
-
-  {% for model_or_source in models_and_sources %}
-    {% if model_or_source.unique_id in unique_ids and dbt_data_privacy.has_data_privacy_meta(model_or_source) %}
-      {% set generated_model = dbt_data_privacy.generate_privacy_protected_model(model_or_source) %}
-      {% if generated_model is not none and generated_model | length > 0 %}
-        {{ generated_models.extend(generated_model) }}
+  {% if unique_ids is not none and unique_ids | length > 0 %}
+    {# Adjust the format
+      A `dbt ls` command returns sources with 'source:xxx'. But, an `unique_id` is like `source.xxx`.
+      TODO consider use `original_file_path`.
+    #}
+    {% set unique_ids = unique_ids | map("replace", "source:", "source.") | list %}
+    {% for model_or_source in models_and_sources %}
+      {% if model_or_source.unique_id in unique_ids
+            and dbt_data_privacy.has_data_privacy_meta(model_or_source) %}
+        {% set generated_model = dbt_data_privacy.generate_privacy_protected_model(model_or_source) %}
+        {% if generated_model is not none and generated_model | length > 0 %}
+          {{ generated_models.extend(generated_model) }}
+        {% endif %}
       {% endif %}
-    {% endif %}
-  {% endfor %}
+    {% endfor %}
+  {% elif original_file_paths is not none and original_file_paths | length > 0 %}
+    {% for model_or_source in models_and_sources %}
+      {% if model_or_source.original_file_path in original_file_paths
+            and dbt_data_privacy.has_data_privacy_meta(model_or_source) %}
+        {% set generated_model = dbt_data_privacy.generate_privacy_protected_model(model_or_source) %}
+        {% if generated_model is not none and generated_model | length > 0 %}
+          {{ generated_models.extend(generated_model) }}
+        {% endif %}
+      {% endif %}
+    {% endfor %}
+  {% else %}
+    {% for model_or_source in models_and_sources %}
+      {% if dbt_data_privacy.has_data_privacy_meta(model_or_source) %}
+        {% set generated_model = dbt_data_privacy.generate_privacy_protected_model(model_or_source) %}
+        {% if generated_model is not none and generated_model | length > 0 %}
+          {{ generated_models.extend(generated_model) }}
+        {% endif %}
+      {% endif %}
+    {% endfor %}
+  {% endif %}
 
   {% if verbose is true %}
     {% do print(tojson(generated_models)) %}
