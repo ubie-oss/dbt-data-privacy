6c8aa020548fce2728f39360038a55b1fe7c037c
Yu Ishikawa <yu-iskw@users.noreply.github.com>
2022-09-29 08:49:29 +0900

    Add `get_tags_by_original_file_paths` (#30)

--- integration_tests/models/restricted_layer/dbt_data_privacy_restricted_layer/users/restricted_layer__dbt_data_privacy_restricted_layer__users.sql
+++ integration_tests/models/restricted_layer/dbt_data_privacy_restricted_layer/users/restricted_layer__dbt_data_privacy_restricted_layer__users.sql
@@ -12,7 +12,7 @@
     labels={
       "modeled_by": "dbt",
     },
-    tags=[],
+    tags=["tag_in_users"],
     grant_access_to=[
       {"project": var('restricted_layer'), "dataset": schema},
       {"project": var('restricted_layer'), "dataset": "dbt_data_privacy_seed"},
