a00755976ca69d489c6ceaa0c69be43f21434d92
Yu ISHIKAWA <yuu.ishikawa+github@gmail.com>
2022-10-18 08:58:48 +0900

    Support dbt 1.3
    Signed-off-by: Yu ISHIKAWA <yuu.ishikawa+github@gmail.com>

--- macros/system/get_default_materialization.sql
+++ macros/system/get_default_materialization.sql
@@ -1,4 +1,4 @@
-{% macro get_default_materialization(data_privacy_config) %}
+{% macro get_default_materialized_config(data_privacy_config) %}
   {% if "default_materialization" not in data_privacy_config %}
     {{ exceptions.raise_compiler_error("'default_materialization' doesn't exist in {}".format(data_privacy_config)) }}
   {% endif %}
