6683134c6d74e9dffdfa4e2bec3d3b1609dd6361
Yu Ishikawa <yu-iskw@users.noreply.github.com>
2025-07-11 09:31:39 +0900

    Improve the integration tests for the legacy `meta` config (#91)

---
+++ integration_tests/macros/tests/test_get_column_meta_block.sql
@@ -0,0 +1,40 @@
+{% macro test_get_column_meta_block() %}
+  {{- return(adapter.dispatch("test_get_column_meta_block", "dbt_data_privacy_integration_tests")()) -}}
+{% endmacro %}
+
+{% macro default__test_get_column_meta_block() %}
+  {# Test for dbt 1.10 or later #}
+  {% set column_1_10 = {
+    "config": {
+      "meta": {"data_privacy": {"level": "confidential"}}
+    }
+  } %}
+  {% set result_1_10 = dbt_data_privacy.get_column_meta_block(column_1_10) %}
+  {% set expected_1_10 = {"data_privacy": {"level": "confidential"}} %}
+  {{ assert_equals(result_1_10, expected_1_10) }}
+
+  {# Test for dbt 1.9 or earlier #}
+  {% set column_1_9 = {
+    "meta": {"data_privacy": {"level": "internal"}}
+  } %}
+  {% set result_1_9 = dbt_data_privacy.get_column_meta_block(column_1_9) %}
+  {% set expected_1_9 = {"data_privacy": {"level": "internal"}} %}
+  {{ assert_equals(result_1_9, expected_1_9) }}
+
+  {# Test for dbt 1.10 or later with empty config.meta but meta has content #}
+  {% set column_empty_config_meta = {
+    "config": {
+      "meta": {}
+    },
+    "meta": {"data_privacy": {"level": "from_meta"}}
+  } %}
+  {% set result_empty_config_meta = dbt_data_privacy.get_column_meta_block(column_empty_config_meta) %}
+  {% set expected_empty_config_meta = {"data_privacy": {"level": "from_meta"}} %}
+  {{ assert_equals(result_empty_config_meta, expected_empty_config_meta) }}
+
+  {# Test when no meta is defined #}
+  {% set column_no_meta = {} %}
+  {% set result_no_meta = dbt_data_privacy.get_column_meta_block(column_no_meta) %}
+  {% set expected_no_meta = none %}
+  {{ assert_equals(result_no_meta, expected_no_meta) }}
+{% endmacro %}
