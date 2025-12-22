98c3bdada8b8bb26329da6d209f0f58340970145
Yu Ishikawa <yu.ishikawa@dr-ubie.com>
2025-12-22 09:54:46 +0900

    fix
    Signed-off-by: Yu Ishikawa <yu.ishikawa@dr-ubie.com>

--- macros/codegen/validate_relationships.sql
+++ macros/codegen/validate_relationships.sql
@@ -1,4 +1,9 @@
 {% macro validate_relationships(relationships) %}
+  {% if relationships is not none and relationships is not sequence %}
+    {# If relationships is not a sequence (e.g., a dict or string), treat it as none #}
+    {{ return(true) }}
+  {% endif %}
+
   {% if relationships is not sequence %}
     {% do exceptions.raise_compiler_error("Invalid relationships: it must be a sequence" ~ relationships) %}
   {% endif %}
