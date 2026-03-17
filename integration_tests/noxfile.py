e18333e5496f5f0757c6ff9bd9b2a7f18b6e5427
Yu Ishikawa <yu.ishikawa@dr-ubie.com>
2026-03-17 15:27:42 +0900

    fix
    Signed-off-by: Yu Ishikawa <yu.ishikawa@dr-ubie.com>

--- integration_tests/noxfile.py
+++ integration_tests/noxfile.py
@@ -1,17 +1,21 @@
 import nox
 import os
+from contextlib import contextmanager
 
 # Nox options
 nox.options.sessions = ["dev_unit_tests"]
 nox.options.default_venv_backend = "uv"
 
 PYTHON_VERSIONS = ["3.10", "3.11", "3.12"]
-DBT_CORE_VERSIONS = ["1.10", "1.11"]
-
-# Mapping of dbt versions to their specific dependencies
-DBT_DEPENDENCIES = {
-    "1.10": ["dbt-core>=1.10,<1.11", "dbt-bigquery>=1.10,<1.11"],
-    "1.11": ["dbt-core>=1.11,<1.12", "dbt-bigquery>=1.11,<1.12"],
+DBT_VERSION_MAP = {
+    "1.10": {
+        "uv_group": "dbt-core-1-10",
+        "description": "dbt-core v1.10",
+    },
+    "1.11": {
+        "uv_group": "dbt-core-1-11",
+        "description": "dbt-core v1.11",
+    },
 }
 
 def get_dataset_name(session, dbt_version):
@@ -51,48 +55,59 @@ def run_cleanup(session, dataset_name, vars_path):
         external=True
     )
 
+@contextmanager
+def dbt_test_env(session, dbt_version):
+    """Context manager to setup and cleanup dbt test environment."""
+    if dbt_version not in DBT_VERSION_MAP:
+        session.error(f"Unsupported dbt version: {dbt_version}")
+
+    version_info = DBT_VERSION_MAP[dbt_version]
+    session.install("--group", version_info["uv_group"])
+
+    dataset_name = get_dataset_name(session, dbt_version)
+    env = {"DBT_DATASET": dataset_name}
+
+    run_deps(session, env)
+
+    vars_path = "resources/vars/vars-bigquery.basic.yml"
+    try:
+        yield {"env": env, "vars_path": vars_path}
+    finally:
+        run_cleanup(session, dataset_name, vars_path)
+
 @nox.session(python="3.12")
 def dev_unit_tests(session):
     """Quickly run unit tests with the latest versions."""
-    unit_tests(session, "1.11")
+    latest_version = list(DBT_VERSION_MAP.keys())[-1]
+    unit_tests(session, latest_version)
 
 @nox.session(python="3.12")
 def dev_integration_tests(session):
     """Quickly run integration tests with the latest versions."""
-    integration_tests(session, "1.11")
+    latest_version = list(DBT_VERSION_MAP.keys())[-1]
+    integration_tests(session, latest_version)
 
 @nox.session(python=PYTHON_VERSIONS)
-@nox.parametrize("dbt_version", DBT_CORE_VERSIONS)
+@nox.parametrize("dbt_version", list(DBT_VERSION_MAP.keys()))
 def unit_tests(session, dbt_version):
     """Run unit tests with specified dbt-core version."""
-    session.install(*DBT_DEPENDENCIES[dbt_version])
-    dataset_name = get_dataset_name(session, dbt_version)
-    env = {"DBT_DATASET": dataset_name}
-    run_deps(session, env)
-
-    vars_path = "resources/vars/vars-bigquery.basic.yml"
-    try:
+    with dbt_test_env(session, dbt_version) as test_env:
         session.run(
             "bash", "run_unit_tests.sh",
             "--target", "bigquery",
-            "--vars-path", vars_path,
-            env=env,
+            "--vars-path", test_env["vars_path"],
+            env=test_env["env"],
             external=True
         )
-    finally:
-        run_cleanup(session, dataset_name, vars_path)
 
 @nox.session(python=PYTHON_VERSIONS)
-@nox.parametrize("dbt_version", DBT_CORE_VERSIONS)
+@nox.parametrize("dbt_version", list(DBT_VERSION_MAP.keys()))
 def integration_tests(session, dbt_version):
     """Run integration tests with specified dbt-core version."""
-    session.install(*DBT_DEPENDENCIES[dbt_version])
-    dataset_name = get_dataset_name(session, dbt_version)
-    env = {"DBT_DATASET": dataset_name}
-    run_deps(session, env)
+    with dbt_test_env(session, dbt_version) as test_env:
+        env = test_env["env"]
+        vars_path = test_env["vars_path"]
 
-    vars_path = "resources/vars/vars-bigquery.basic.yml"
-    try:
         # Modern generation and build
         session.run(
             "bash", "scripts/generate_secured_models.sh",
@@ -125,8 +140,17 @@ def integration_tests(session, dbt_version):
             env=env,
             external=True
         )
-    finally:
-        run_cleanup(session, dataset_name, vars_path)
+
+@nox.session(python=PYTHON_VERSIONS)
+@nox.parametrize("dbt_version", list(DBT_VERSION_MAP.keys()))
+def setup_dbt_env(session, dbt_version):
+    """Setup a virtual environment for a specific dbt version and output its bin path."""
+    if dbt_version not in DBT_VERSION_MAP:
+        session.error(f"Unsupported dbt version: {dbt_version}")
+
+    version_info = DBT_VERSION_MAP[dbt_version]
+    session.install("--group", version_info["uv_group"])
+    print(f"BIN_PATH={session.bin}")
 
 @nox.session(python=PYTHON_VERSIONS)
 def dbt_fusion_tests(session):
